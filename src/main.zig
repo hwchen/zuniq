27f6f758712cce35e327c09ae62d3fb5c6f58a5a
Walther Chen <walther.chen@gmail.com>
2022-12-20 23:40:35 -0500

    basic filtering

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,40 @@
 const std = @import("std");
+const Set = std.StringHashMap(void);
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var alloc = arena.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    var filter = Set.init(alloc);
+
+    const stdin_file = std.io.getStdIn().reader();
+    var br = std.io.bufferedReader(stdin_file);
+    const stdin = br.reader();
+
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var buf: [1024 * 8]u8 = undefined;
+
+    while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (filter.contains(line)) {
+            continue;
+        } else {
+            var key = try alloc.alloc(u8, line.len);
+            std.mem.copy(u8, key, line);
+            try filter.put(key, {});
+            try stdout.print("{s}\n", .{line});
+        }
+
+        // debug
+        //std.debug.print("line: {s}\n", .{line});
+        //var key_it = filter.keyIterator();
+        //while (key_it.next()) |key| {
+        //    std.debug.print("  key: {s}\n", .{key.*});
+        //}
+    }
 
     try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
